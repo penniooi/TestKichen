e80fc91f26ae06d18ef915aa44be8e3a330526a8
“hahaxiaoniuniu12” <hujieyu4@163.com>
2016-08-15 10:11:01 +0800

    初始化项目

---
+++ TestKitchen/TestKitchen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestKitchen
+//
+//  Created by aloha on 16/8/15.
+//  Copyright © 2016年 胡颉禹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
